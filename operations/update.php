a91ad00d062a2ccb81969467437c8efb6b42db09
NITESH KUMAR <126846212+niteshkumar789@users.noreply.github.com>
2025-10-24 19:27:41 +0530

    Enhance update.php to include photo upload
    Added photo upload functionality to the update process.

--- operations/update.php
+++ operations/update.php
@@ -1,21 +1,36 @@
 <?php
+include '../config/db.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ID'])) {
-    include '../config/db.php';
     $id = (int)$_POST['ID'];
     $name = $conn->real_escape_string($_POST['NAME']);
     $email = $conn->real_escape_string($_POST['EMAIL']);
 
-    $sql = "UPDATE users SET NAME='$name', EMAIL='$email' WHERE ID=$id";
-    echo $conn->query($sql) ? "Updated Successfully!" : "Error: " . $conn->error;
+    $photoPath = NULL;
+    if (isset($_FILES['PHOTO']) && $_FILES['PHOTO']['error'] === 0) {
+        $uploadDir = '../empPhotos/';
+        $filename = time() . '_' . basename($_FILES['PHOTO']['name']);
+        $targetFile = $uploadDir . $filename;
+
+        if (move_uploaded_file($_FILES['PHOTO']['tmp_name'], $targetFile)) {
+            $photoPath = "empPhotos/$filename";
+        }
+    }
+
+    $sql = "UPDATE users SET NAME='$name', EMAIL='$email'" . ($photoPath ? ", photo='$photoPath'" : "") . " WHERE ID=$id";
+    echo $conn->query($sql) ? "Updated successfully!" : "Error: " . $conn->error;
     $conn->close();
     exit;
 }
 ?>
 
-<form id="updateForm">
+<form id="updateForm" enctype="multipart/form-data">
   <h3>Update Employee</h3>
   <input type="number" name="ID" placeholder="Enter ID" required><br>
   <input type="text" name="NAME" placeholder="New Name" required><br>
   <input type="email" name="EMAIL" placeholder="New Email" required><br>
+  <input type="file" name="PHOTO" accept="image/*" id="updatePhoto"><br>
+  <img id="updatePreview" src="" alt="Photo Preview" style="display:none; width:80px; height:80px; margin-top:5px;"><br>
   <button type="submit">Update</button>
 </form>
+
