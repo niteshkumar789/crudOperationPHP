88f1331e59befc90ee8f561d17050018f0a375d6
NITESH KUMAR <126846212+niteshkumar789@users.noreply.github.com>
2025-10-24 19:25:52 +0530

    Add multi-delete functionality to employee table

--- operations/fetch.php
+++ operations/fetch.php
@@ -4,15 +4,37 @@ include '../config/db.php';
 $result = $conn->query("SELECT * FROM users");
 
 if ($result->num_rows > 0) {
-    echo "<h3>Current Employee Table</h3>";
-    echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>{$row['ID']}</td><td>{$row['NAME']}</td><td>{$row['EMAIL']}</td></tr>";
-    }
-    echo "</table>";
-} else {
-    echo "<p>No records found.</p>";
-}
+  echo "<h3>Current Employee Table</h3>";
+  echo "<form id='multiDeleteForm'>";
+  echo "<table>
+        <tr>
+          <th><input type='checkbox' id='selectAll'> Select All</th>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Photo</th>
+          <th>Action</th> <!-- changed from empty to 'Action' -->
+        </tr>";
 
+  while ($row = $result->fetch_assoc()) {
+    $photo = $row['photo'] ? "<img src='{$row['photo']}' width='50' height='50'>" : "No Photo";
+    echo "<tr>
+            <td><input type='checkbox' class='recordCheckbox' name='ids[]' value='{$row['ID']}'></td>
+            <td>{$row['ID']}</td>
+            <td>{$row['NAME']}</td>
+            <td>{$row['EMAIL']}</td>
+            <td>$photo</td>
+            <td><button type='button' class='editBtn' data-id='{$row['ID']}'>Edit</button></td>
+          </tr>";
+  }
+
+
+  echo "</table>
+        <br>
+        <button type='submit'>Delete Selected</button>
+        </form>";
+} else {
+  echo "<p>No records found.</p>";
+}
 $conn->close();
 ?>
